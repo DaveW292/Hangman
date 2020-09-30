b59c6d0a43f14ff1b6ebe2e4756527b0772406e1
davew292 <davew@tuta.io>
2020-09-30 19:57:29 +0200

    sounds & jangman.js implemented

---
+++ hangman.js
@@ -0,0 +1,7 @@
+var pass = "A room without books is like a body without a soul";
+pass = pass.toUpperCase();
+var passLength = pass.length;
+var hiddenPass = "";
+var mistakes = 0;
+var yes = new Audio("sounds/yes.mp3");
+var no = new Audio("sounds/no.mp3");
